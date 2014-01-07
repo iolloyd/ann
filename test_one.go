d3b2224e72259e835b9925a28fcba99a03ce0c04
lloyd <manchesterboy@gmail.com>
2014-01-07 15:46:24 +0100

    add test file

---
+++ test_one.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"./nn"
+)
+
+var (
+	inSize     = 4
+	hiddenSize = 4
+	outSize    = 2
+	iterations = 100
+	errorThreshold = 0.1
+)
+
+func main() {
+	td := nn.TrainingData{}
+	td.Inputs = [][]float64{
+		{0.0, 0.0, 0.0, 0.0},
+		{0.0, 0.0, 0.0, 1.0},
+		{1.0, 0.0, 1.0, 0.0}}
+
+	td.Outputs = [][]float64{
+		{0.0, 0.0},
+		{1.0, 0.0},
+		{0.0, 1.0}}
+
+	nn := nn.MakeNN(inSize, hiddenSize, outSize, errorThreshold)
+	nn.Train(td, iterations)
+	nn.Show()
+}

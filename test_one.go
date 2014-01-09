0faed7109ed1017cab42f40892fb24ba40fb007e
lloyd <manchesterboy@gmail.com>
2014-01-09 10:14:19 +0100

    some refactoring and correcting layer building

--- test_one.go
+++ test_one.go
@@ -14,15 +14,8 @@ var (
 
 func main() {
 	td := nn.TrainingData{}
-	td.Inputs = [][]float64{
-		{0.0, 0.0, 0.0, 0.0},
-		{0.0, 0.0, 0.0, 1.0},
-		{1.0, 0.0, 1.0, 0.0}}
-
-	td.Outputs = [][]float64{
-		{0.0, 0.0},
-		{1.0, 0.0},
-		{0.0, 1.0}}
+	td.Inputs = [][]float64{{9.0, 1.0, 0.0, 1.0}}
+	td.Outputs = [][]float64{{0.0, 1.0}}
 
 	nn := nn.MakeNN(inSize, hiddenSize, outSize, errorThreshold)
 	nn.Train(td, iterations)
